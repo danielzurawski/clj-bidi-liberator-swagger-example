16918b2bb1a86686a848b08582050ffdac8c8712
Daniel Żurawski <zurawski.daniel@gmail.com>
2020-05-02 12:39:32 +0100

    feat(): compare bidi and compojure

--- project.clj
+++ project.clj
@@ -3,11 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-ring "0.12.2"]]
-  :ring {:handler clj-bidi-liberator-swagger-example.core/handler}
+  :plugins [[lein-ring "0.12.5"]]
+
+  ;:ring {:handler clj-bidi-liberator-swagger-example.core/compojure-handler}
+  :ring {:handler clj-bidi-liberator-swagger-example.core/bidi-handler}
+
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [liberator "0.15.3"]
                  [bidi "2.1.6"]
                  [compojure "1.6.0"]
-                 [ring/ring-core "1.6.3"]]
+                 [ring/ring-core "1.7.1"]]
   :repl-options {:init-ns clj-bidi-liberator-swagger-example.core})
--- src/clj_bidi_liberator_swagger_example/core.clj
+++ src/clj_bidi_liberator_swagger_example/core.clj
@@ -1,11 +1,46 @@
 (ns clj-bidi-liberator-swagger-example.core
-  (:require [liberator.core :refer [resource defresource]]
-            [ring.middleware.params :refer [wrap-params]]
-            [compojure.core :refer [defroutes ANY]]))
+  (:require
+    [liberator.core :refer [resource defresource]]
+    [ring.middleware.params :refer [wrap-params]]
+    [compojure.core :refer [defroutes ANY]]
+    [bidi.ring :refer [make-handler]]))
 
-(defroutes app
-  (ANY "/" [] (resource)))
+;; Domain logic
+(defn print-text [text]
+  (format "The text is %s" text))
 
-(def handler
-  (-> app
-      wrap-params))
+
+;; Liberator resources
+; Compojure
+(defresource for-compojure-resource [txt]
+  :available-media-types ["text/plain"]
+  :exists? (fn [_] txt)
+  :handle-ok (fn [_] (print-text txt)))
+
+; Bidi
+(defresource for-bidi-resource [print-text-fn]
+  :available-media-types ["text/plain"]
+  :exists? (fn [ctx] {::text (get-in ctx [:request :route-params :text])})
+  :handle-ok (fn [ctx] (print-text-fn (::text ctx))))
+
+
+;; Routes
+; Compojure
+(defroutes compojure-routes
+  (ANY "/foo/:txt" [txt] (for-compojure-resource txt)))
+
+; Bidi
+(def bidi-routes ["/foo/" {[:text] (for-bidi-resource print-text)}])
+
+
+;; Ring Handler
+; Compojure
+(def compojure-handler
+  (-> compojure-routes
+    wrap-params))
+
+; Bidi
+(def bidi-handler
+  (->
+    (make-handler bidi-routes)
+    wrap-params))
